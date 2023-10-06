22ae824c1cddc15414904ad684505a9675a54117
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-06 16:52:27 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C code porgramming that code while loop function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch)
+		ch++
+	}
+        putchar('\n');
+	return (0);
+}
