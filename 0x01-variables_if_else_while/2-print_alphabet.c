a29a425910e49bfec990102a6ab543dee0d0d315
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-06 14:58:11 -0700

    2-print_alphabet.c task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C code porgramming that code while loop function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+	return (0);
+
+
+}
