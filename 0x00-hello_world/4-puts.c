a71779ce3dbbe0c73ab4b723c21cf3a135addbcd
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-05 23:29:16 -0700

    1-compiler task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/**
+/*
  *programming in puts 
  *just use for string
  *description a cprogram by puts function
 */
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
