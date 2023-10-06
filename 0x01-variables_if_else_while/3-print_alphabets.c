7e72f6464b92bc8f0e524ad3625fde513571cd11
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-06 16:35:52 -0700

    3-print_alphabets.c task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,21 +12,18 @@
 
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 	char CH = 'A';
 
-        while (ch <= 'z')
-        {
-                putchar(ch);
-                ch++;
-        }
-                
-       
-	
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
 	while (CH <= 'Z')
 	{
-		putchar(CH);
-		CH++;
+	putchar(CH);
+	CH++;
 	}
 	putchar('\n');
 	return (0);
