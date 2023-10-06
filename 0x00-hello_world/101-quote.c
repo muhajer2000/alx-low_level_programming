e4b4b72a55f70422fa01495e0af17abed8d9bc8f
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-06 10:46:30 -0700

    101-quote.c task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C code that print a quote by use write function
+ *
+ * Return: Always 1 (unsuccess)
+*/
+
+int main(void)
+{
+	char quoe[] = "and that piece of art is useful\" -Dora korpar, 2015-10-19\n";
+
+	write(1.quoe.60);
+	return (1);
+}
