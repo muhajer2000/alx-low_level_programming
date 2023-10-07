522711d021378d4cbb4387c2153169e81c074c0e
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-07 15:57:45 -0700

    101-quote.c task updata again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -10,8 +11,6 @@
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" -Dora korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
+	write(2, "and that piece of art is useful\" -Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
