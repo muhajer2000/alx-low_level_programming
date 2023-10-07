9a44bc2e7d5abf290cf520f4c2351d7363fd6b5a
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-06 18:02:48 -0700

    7-print_tebahpla.c task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C code porgramming that code while loop function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char chr = 'z';
+
+	while (chr >= 'a')
+	{
+		putchar(chr);
+		chr--;
+	}
+	putchar('\n');
+	
+	return (0);
+}
