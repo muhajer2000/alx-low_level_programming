610b2b24c3bf65cb7dad2c27652d5eb553cd5170
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-06 19:17:16 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,13 +14,13 @@ int main(void)
 {
 	int num1 = 0, num2;
 
-	while (num1 <= 10)
+	while (num1 <= 9)
 	{
 		num2 = 0;
 
-		while (num2 <= 10)
+		while (num2 <= 9)
 		{
-			if(num1 != num2 && num1 < num2)
+			if(num1 != num2 && num1 + num2)
 			{
 				putchar(num1 = 48);
 				putchar(num2 = 48);
