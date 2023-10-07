52b95c39fcd899d20d91ac6674f66158dc10a1e4
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-06 18:14:41 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C code porgramming that code while loop function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 102)
+	{
+		putchar(num);
+
+		if (num == 57)
+			num += 39;
+		num++;
+	}
+	putchar('\n')
+	
+	return (0);
+}
