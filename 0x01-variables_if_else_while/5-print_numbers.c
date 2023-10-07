bbe0874051c983f8a73528996fd46bf0818ba83d
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-06 17:19:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C code porgramming that code while loop function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+	while (num <= 10)
+	{
+		printf("%d",num);
+		num++;
+	}
+		printf('\n');
+	return (0);
+}
