56af33e3ec358bf552d3348b8353f2b373a01146
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-09 08:46:07 -0700

    102-print_comb5.c task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C code programming print all
+ *                 possible combanation of two two digit
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int firstnum = 0, scenum;
+	while (firstnum <= 99)
+	{
+		scenum = 0;
+		while (scenum <= 99)
+		{
+			if (scenum != firstnum)
+			{
+				putchar((firstnum / 10) + 48);
+				putchar((firstnum % 10) + 48);
+				putchar(' ');
+				putchar((scenum / 10) + 48);
+				putchar((scenum % 10) + 48);
+
+				if (firstnum != 98 || scenum != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			scenum++;
+		}
+		firstnum++;
+	}
+	putchar('\n');
+	return (0);
+}
