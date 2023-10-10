b8d4be0298e8a40c938e84b0f637bd945bb8071f
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-10 14:04:45 -0700

    0-putchar.c updata 2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 
 #include <stdio.h>
-int _putchar(char);
+
+int _putchar(char C);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -16,4 +17,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+#endif 
