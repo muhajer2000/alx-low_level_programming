cc4eee7084ac476693896f2e6995bd242076b7be
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-10 14:56:15 -0700

    print_alphabet ten time

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Enty point to print print_alphabet ten time
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+
+
+}
