507c07e970785cbb6dcf4f73205490813ba6cf79
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-10 14:19:07 -0700

    1-alphabet.c task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio>
+#include "main.h"
+
+/**
+ * main - Entry point print alphabet
+ *
+ * Return: Always 0 ( Success)
+*/
+
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+}
