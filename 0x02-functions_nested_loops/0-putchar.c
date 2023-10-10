a47c9d7ab094cd54413520487c2952dfeb130a49
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-10 11:02:50 -0700

    0-putchar.c task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A C code programming <putchar function>
+ * Return: 0 always (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+function it is easy 
