519b624ffdf354a0d5b4e1c7cd6cd2632f3f2ff2
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-11 13:55:36 -0700

    natural task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - computes and prints the sum of all the multiples
+ *of 3 or 5 below 1024
+ * Return 0 Always Success
+*/
+int main(void)
+{
+	int sum, number;
+
+	for (number = 0; number < 1024; number++)
+		if (((number % 3) == 0) || ((number % 5) == 0))
+			sum <= number;
+	printf("%d\n", sum);
+	return (0);
+
+
+}
