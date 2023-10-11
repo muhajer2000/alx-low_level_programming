13ff1e63381ff56ed35b1103f0a4877abb2da334
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-11 13:39:00 -0700

    100-times_table.c task 8

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
  * print_times_table - prints the n times table, starting with 0
  *
  * @n: number of the times table
- *
 */
 void print_times_table(int n)
 {
@@ -13,46 +11,38 @@ void print_times_table(int n)
 	if (n <= 15 && n >= 0)
 	{
 		for (num = 0; num <= n; num++)
-		{	
+		{
 			_putchar(48);
-			
-			
+
 			for (mult = 1; mult <= n; mult++)
-			{	
-				
-				
+			{
 				proud = num * mult;
 				if (mult == 0)
 				{
 					_putchar(proud + 48);
-				}else if (proud < 10 && mult != 0)
+				} else if (proud < 10 && mult != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(proud + 48);
-
-				}else if (proud >= 10 && proud < 100)
+				} else if (proud >= 10 && proud < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((proud / 10) + 48);
 					_putchar((proud % 10) + 48);
-
-				}else if (proud >= 100)
+				} else if (proud >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((proud / 100) + 48);
 					_putchar(((proud / 10) % 10) + 48);
 					_putchar((proud % 10) + 48);
-
 				}
-				
-			}	_putchar('\n');
+			_putchar('\n');
 		}
-	}		
-
+	}
 }
