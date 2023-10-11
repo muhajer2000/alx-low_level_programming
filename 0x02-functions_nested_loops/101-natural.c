9cbb65cf73e51a55136ce92b01da3348e079274f
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-11 14:17:04 -0700

    101-natural.c task 3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,15 +8,25 @@
 */
 int main(void)
 {
-	int sum, number;
+	int sum3, sum5, sum, number;
+	
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
 
-	for (number = 0; number < 1024; number++)
+	for (number = 0; number < 1024; ++number)
 	{	
-		if (((number % 3) == 0) || ((number % 5) == 0))
-		{	
-			sum == number;
+		if ((number % 3) == 0)
+		{
+			sum3 = sum3 + number;
+		} else if ((number % 5) == 0)
+		{
+			sum5 = sum5 + number
+		
 		}
+		
 	}
+	sum = sum3 + sum5
 	printf("%d\n", sum);
 	return (0);
 
