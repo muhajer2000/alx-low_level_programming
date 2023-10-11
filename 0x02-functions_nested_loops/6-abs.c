09c00c59c41d143c77436a5e375f0212a7aa22f0
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-11 02:20:46 -0700

    int _abs(int) task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs* - absolute value of an intger
+ * @n: the check of int
+ * Retutn: the value of int
+*/
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
