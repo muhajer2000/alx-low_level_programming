f0c25b3f4a4b4a5bc5bafbf07b75f30014ce9afd
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-12 16:56:20 -0700

    5-more_numbers.c task 1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,19 @@
 */
 void more_numbers(void)
 {
-	int line;
+	int num, row, count;
 
-	for (line = 0; line < 10; line++)
+	for (row = 1; row <= 10; row++)
 	{
-		if (line == 2 || line == 4)
-			continue;
-		_putchar(line + 48);
-
-	}
-	_putchar('\n');
-
+		for (count = 0; count <= 14; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+	}	_putchar('\n');
 }
