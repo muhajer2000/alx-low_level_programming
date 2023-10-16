af695cee635f932f5fab450fb877b11fb198d9ad
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-16 12:56:46 -0700

    2-strlen.c task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main,h"
+
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: string of value
+ *
+ * Return: the length of the string
+*/
+int _strlen(char *s)
+{
+	int contuner;
+
+	for (contuner = 0; s[contuner] != '\0'; s++)
+	{
+		contuner++;
+	}
+	return (contuner);
