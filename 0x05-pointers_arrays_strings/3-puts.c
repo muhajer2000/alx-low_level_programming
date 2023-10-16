b91141679b23196f17b06d89b6a4e1f2da2ce54e
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-16 13:51:05 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - print string to stdout
+ * @str: pointer to the print string
+*/
+void _puts(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+
