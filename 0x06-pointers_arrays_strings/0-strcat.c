bc8aa4a6f03e9a6f330bfbc6a22c33ba8b96c7ff
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-19 14:16:56 -0700

    0-strcat.c task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,14 +12,22 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	i = 0
+	i = 0;
+	j = 0;
 
-	while (dest[i])
-
+	while (dest[i] != '\0')
 		i++;
-	for (j = 0; src[j]; j++)
 
-		dest[i++] = src[j];
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+
+	}
+
+	dest[i] = '\0';
+
 	return (dest);
 
 }
