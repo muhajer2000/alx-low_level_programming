bffcb0f4cca474aa17fa33b0599e88d3338dd904
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-27 15:27:53 -0700

    1-args.c task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument to print number of it
+ * @argv: array of string of argument
+ * Return: 0 success
+ *
+*/
+int main(int argc, char *argv[])
+{
+	printf("i\n", argc);
+	return (0);
+}
