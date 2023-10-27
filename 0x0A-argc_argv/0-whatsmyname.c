996500d9ffe3ad1dbd58a91cd40719d7e54de982
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-27 15:03:18 -0700

    0-whatsmyname.c task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument that store size of array
+ * @argv: string of array arguments
+ * Return: Always 0 success
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+}
