193f4333495ac457513e19ccad5987fe2d3e7b8f
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-28 01:43:52 -0700

    task mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _atoi(char *s);
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @s: pointer point to numbers
+ * Return: the int converted from the string
+*/
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int digit = 1;
+
+	do {
+		if (*s == '-')
+			digit *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+		} while (*s++);
+		return (num * digit);
+}
+
 /**
  * main - Entry point
  * @argc: number of arguments
@@ -11,8 +34,8 @@ int main(int argc, char *argv[])
 {
 	int mul1, mul2, result;
 
-	mul1 = atoi(argv[1]);
-	mul2 = atoi(argv[2]);
+	mul1 = _atoi(argv[1]);
+	mul2 = _atoi(argv[2]);
 
 	if (argc <= 3)
 	{
