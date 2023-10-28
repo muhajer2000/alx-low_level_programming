745bc4051243b2d705f92c56bf920d587f24be02
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-27 18:16:41 -0700

    task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: string of arguments
+ * Return: 0 success 1 error
+*/
+int main(int argc, char *argv[])
+{
+	int coins[] = {25, 10, 5, 2, 1};
+	int i, cent = 0;
+	int amount = atoi(argv[1]);
+
+	if  (argc == 2)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (amount >= coins[i])
+			{
+				cent += amount / coins[i];
+				amount = amount % coins[i];
+				if (amount % coins[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+	printf("%d\n", cent);
+	}
+	else if (argc == 0 || argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
