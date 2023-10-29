019eb8e6ed2d361343ecb60607a13502329ab41d
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-29 14:58:01 -0700

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,40 +1,40 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
 /**
  * main - Entry point
  * @argc: number of arguments
  * @argv: string of arguments
- * Return: 0 success 1 error
+ * Return: Always 0 success 1 error
 */
 int main(int argc, char *argv[])
 {
+	int money, i, amount;
 	int coins[] = {25, 10, 5, 2, 1};
-	int i, cent = 0;
 
-	if  (argc == 2)
-	{
-		for (i = 0; i < 5; i++)
-		{
-			int amount;
-
-			if (amount >= coins[i])
-			{
-				amount = atoi(argv[1]);
-				cent += amount / coins[i];
-				amount = amount % coins[i];
-				if (amount % coins[i] == 0)
-				{
-					break;
-				}
-			}
-		}
-	printf("%d\n", cent);
-	}
-	else if (argc > 0 || argc > 2)
+	if (argc > 2 || argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	money = atoi(argv[1]);
+	amount = 0;
+
+	if (money < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < 5 && money >= 0; i++)
+	{
+		while (money >= coins[i])
+		{
+			amount++;
+			money -= coins[i];
+		}
+	}
+	printf("%d\n", amount);
 	return (0);
 }
