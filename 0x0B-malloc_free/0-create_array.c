7f4a3b5210c150921c621c37cc538198d2484752
muhajer2000 <muhajeribrahim4@gmail.com>
2023-10-30 19:46:27 -0700

    0-create_array. task 2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,19 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	ptr = (char *) malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (0);
 
 	for (i = 0; i < size; i++)
 	{
-		ptr[i] = 'c';
+		*(ptr + i) = c;
 
 	}
+	*(ptr + i) = '\0';
 	return (ptr);
 }
