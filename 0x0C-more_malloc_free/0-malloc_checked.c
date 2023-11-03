548964e161510c8bb001be51edc2ac413ea08f5d
muhajer2000 <muhajeribrahim4@gmail.com>
2023-11-02 20:24:24 -0700

    0-malloc_checked.c task 3

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,7 +7,7 @@
  * @b: is variable that allocat in memory
  * Return: NULL if malloc fail and tremaiante by 98
 */
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
 	int *contr;
 
