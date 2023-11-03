41e061232adf598157e002fa7627eddf74e45a9d
muhajer2000 <muhajeribrahim4@gmail.com>
2023-11-03 00:07:33 -0700

    2-calloc.c task 5

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,21 +2,6 @@
 #include <string.h>
 
 /**
- * _nmem - function fill memory
- * @str: string of pointer
- * @i: character that fill by
- * @n: number of character
- * Return: 0 success and str if NULL
-*/
-char *_nmem(char *str, char i, unsigned int n)
-{
-	char *s = str;
-
-	while (n--)
-		*str++ = i;
-	return (str);
-}
-/**
  * _calloc - a function that allocates memory for an array
  * @nmemb: number of elements
  * @size: size of bytes to store
