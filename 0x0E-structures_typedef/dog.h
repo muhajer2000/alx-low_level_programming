9fe4a5b78a66e09edaafa61a45c158d453e96da1
muhajer2000 <muhajeribrahim4@gmail.com>
2023-11-06 15:50:51 -0800

    dog.h task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - struct of dog proprites
+ * Description: list of dog information by structure
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /*dog.h*/
