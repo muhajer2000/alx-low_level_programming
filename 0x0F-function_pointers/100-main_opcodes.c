9408b64d00d661730794660f8a6fa67c31df704b
muhajer2000 <muhajeribrahim4@gmail.com>
2023-11-08 18:27:10 -0800

    100-main_opcodes.c task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * main - Entry point
+ * @argc: number of argument
+ * @argv: string of argument
+ * Return: 0 (success)
+*/
+int main(int argc, char **argv)
+{
+	int a;
+	char *ptr = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atoi(argv[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (a--)
+		printf("%02hhx%s", *p++, a ? " " : "\n");
+	return (0);
+}
