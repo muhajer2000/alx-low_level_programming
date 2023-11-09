5a5402959fcfbcc655d6057eb05ec937f4fe4794
muhajer2000 <muhajeribrahim4@gmail.com>
2023-11-08 18:06:18 -0800

    3-main.c task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,19 +13,17 @@ int main(int argc, char **argv)
 	int (*func_opr)(int, int);
 
 	if (argc != 4)
-		printf("Error\n");
-		exit(98);
+		printf("Error\n"), exit(98);
 
 	x = atoi(argv[1]);
 	y = atoi(argv[3]);
 
 	func_opr = get_op_func(argv[2]);
 	if (!func_opr)
-		printf("Error\n");
-		exit(99);
+		printf("Error\n"), exit(99);
+
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && !y)
-		printf("Error\n");
-		exit(100);
+		printf("Error\n"), exit(100);
 
 	printf("%d\n", func_opr(x, y));
 	return (0);
