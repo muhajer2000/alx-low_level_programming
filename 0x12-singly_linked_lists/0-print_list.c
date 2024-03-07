687823a40159b3514fa594e151c9850f112c213b
muhajer <muhajeribrahim4@gmail.com>
2024-03-07 13:40:20 -0800

    0-print_list.c task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,16 @@ size_t print_list(const list_t *h)
 {
 	int i = 0;
 	unsigned int n = 0;
-    
-	while(h != NULL)
+
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] %s\n", h->str);
 		else
-			printf("[%d] %s\n",h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
 		n++;
 	}
-	return(n);
+	return (n);
 }
